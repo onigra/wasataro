6a5955ea4caa6ac015639ba7a89ee018723cfd5d
onigra <3280467rec@gmail.com>
2021-01-02 19:14:57 +0900

    add usage test

---
+++ usage_test.go
@@ -0,0 +1,49 @@
+package wasataro_test
+
+import (
+	"bytes"
+	"io"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/onigra/wasataro"
+)
+
+func TestUsage(t *testing.T) {
+	// Setup
+	// catch stderr
+	r, w, err := os.Pipe()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	stderr := os.Stderr
+	os.Stderr = w
+
+	// And
+	expected := `Usage: wasataro [<flags>]
+
+Flags:
+  -response String default: ok
+  -port     Int    default: 3000`
+
+	// when
+	f := wasataro.Usage()
+	f()
+
+	// Teardown
+	os.Stderr = stderr
+	w.Close()
+
+	var buf bytes.Buffer
+	io.Copy(&buf, r)
+
+	// then
+	raw := buf.String()
+	actual := strings.TrimRight(raw, "\n")
+
+	if actual != expected {
+		t.Errorf("actual: %s, expected: %s", actual, expected)
+	}
+}
